5893dbfd9825e6df7c3ad1aed08ff4b19dca325b
NekoChan <aaron74327@gmail.com>
2024-11-16 22:21:15 -0800

    init

---
+++ app.config.ts
@@ -0,0 +1,18 @@
+import { defineConfig } from "@tanstack/start/config";
+import tsConfigPaths from "vite-tsconfig-paths";
+
+export default defineConfig({
+  server: {
+    preset: "cloudflare-pages",
+    rollupConfig: {
+      external: ["node:async_hooks"],
+    },
+  },
+  vite: {
+    plugins: [
+      tsConfigPaths({
+        projects: ["./tsconfig.json"],
+      }),
+    ],
+  },
+});
